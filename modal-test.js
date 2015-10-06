bf793f83dc9996d933222a5f9ad81d9ad4bf780e
Mark Solters <msolters@driblet.io>
2015-10-06 13:15:30 -0400

    Created detailed example and walk-through.

--- modal-test.js
+++ modal-test.js
@@ -1,5 +1,23 @@
 if (Meteor.isClient) {
+
+  //  (1) Create a reactive-var.
+  //      Make sure we can access it from the console.
+  window.exampleMessage = new ReactiveVar();
+  //  (2) Set it to a value.
+  window.exampleMessage.set("Hello world!");
+
+  //  (3) Create a MaterializeModal.
   Template.body.rendered = function() {
-    MaterializeModal.alert();
+    MaterializeModal.message({ bodyTemplate: "reactiveModal", title: "Reactive Modal Example!", exampleMessage: window.exampleMessage });
   };
+
+  //  (4) We just create simple .get() helpers to grab the
+  //      value of any reactive-vars we passed into our
+  //      MaterializeModal.
+  Template.reactiveModal.helpers({
+    exampleMessage: function() {
+      return this.exampleMessage.get();
+    }
+  });
+
 }
